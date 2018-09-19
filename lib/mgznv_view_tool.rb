1b00260297ed856af7420bf0195d0cb4a983d403
Manuel Guzman <leunam.toshi@gmail.com>
2018-09-19 17:17:50 -0500

    Dev tool initial commit

---
+++ lib/mgznv_view_tool.rb
@@ -0,0 +1,6 @@
+require "mgznv_view_tool/version"
+require "mgznv_view_tool/renderer"
+
+module MgznvViewTool
+  # Your code goes here...
+end
---
+++ lib/mgznv_view_tool/version.rb
@@ -0,0 +1,3 @@
+module MgznvViewTool
+  VERSION = "0.1.0"
+end
